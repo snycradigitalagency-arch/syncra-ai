62db0ef6c4e5c371985c93c7e9e5e8253c6760c7
snycradigitalagency-arch <snycradigitalagency@gmail.com>
2026-05-26 09:26:19 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy8fSEgulQrrzwhfNFHSIegWlLCAf9tqQik",
+  authDomain: "syncra-ai-6b4dd.firebaseapp.com",
+  projectId: "syncra-ai-6b4dd",
+  storageBucket: "syncra-ai-6b4dd.firebasestorage.app",
+  messagingSenderId: "572449909275",
+  appId: "1:572449909275:web:e8b8f0b4adc945c33e26be",
+  measurementId: "G-BQMG6S7F7N"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
